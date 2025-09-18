aef6aff9aadf3b57ffbc18d223904fac26bc14a5
Andrey Andreev <collapsegamer@gmail.com>
2025-09-18 14:03:36 +0300

    fix: статические файлы

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -133,7 +133,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = [BASE_DIR / 'collected_static']
+STATIC_ROOT = [BASE_DIR / 'staticfiles/static']
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
