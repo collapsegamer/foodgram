a28acbebf3461d4880237495307ec0c343640beb
Andrey Andreev <collapsegamer@gmail.com>
2025-09-19 16:48:27 +0300

    fix: signup

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -32,7 +32,7 @@ class UserCreateSerializer(serializers.ModelSerializer):
         fields = ('email', 'username', 'first_name', 'last_name', 'password')
         extra_kwargs = {'password': {'write_only': True}}
 
-    def create(self, validated):
+    def create(self, validated_data):
         return User.objects.create_user(**validated_data)
 
 
