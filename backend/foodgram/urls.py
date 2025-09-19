db4341cae91c4bbaa6a7e5af337db2c0a0a435fe
Andrey Andreev <collapsegamer@gmail.com>
2025-09-19 17:07:26 +0300

    fix: signin

--- backend/foodgram/urls.py
+++ backend/foodgram/urls.py
@@ -6,10 +6,11 @@ from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('api.urls')),
-    path('auth/', include('djoser.urls')),
-    path('auth/', include('djoser.urls.authtoken')),
+    
+    path('api/auth/', include('djoser.urls')),
+    path('api/auth/', include('djoser.urls.authtoken')),
 
+    path('api/', include('api.urls')),
     path('api/docs/',
          TemplateView.as_view(template_name='redoc.html'), name='redoc'),
     path(
