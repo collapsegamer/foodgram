a5c81d6b4154c7796baf2e8357d2b7225e502329
Andrey Andreev <collapsegamer@gmail.com>
2025-09-20 17:46:50 +0300

    fix: static

--- backend/foodgram/urls.py
+++ backend/foodgram/urls.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls.static import static
 from django.views.generic import TemplateView
 from django.views.static import serve
 from django.conf import settings
@@ -18,3 +19,8 @@ urlpatterns = [
         name='openapi-schema'
     ),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL,
+                          document_root=settings.MEDIA_ROOT
+                          )
